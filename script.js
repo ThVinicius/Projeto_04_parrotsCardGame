52ea96dc0834691acfd7b1016efd2a3ea8332f2f
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-04-10 10:22:11 -0300

    Restart concluído

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function comparador() {
-  return Math.random() - 0.5
-}
 let quantidadeCartas
 let meuIntervalo
+let cartas = []
+let clicks = 0
+let index1
+let index2
+inicioJogo()
+criarArray()
+
 function inicioJogo() {
   do {
     quantidadeCartas = prompt(
@@ -18,34 +22,30 @@ function inicioJogo() {
   document.querySelector('img').classList.add('escondido')
   let contador = 0
   function cronometro() {
-    console.log('cronometro')
     contador++
     document.querySelector('h2').innerHTML = `${contador}`
   }
   meuIntervalo = setInterval(cronometro, 1000)
 }
-inicioJogo()
-
-let imagens = [
-  '/images/bobrossparrot.gif',
-  '/images/explodyparrot.gif',
-  '/images/fiestaparrot.gif',
-  '/images/metalparrot.gif',
-  '/images/revertitparrot.gif',
-  '/images/tripletsparrot.gif',
-  '/images/unicornparrot.gif'
-]
-imagens.sort(comparador)
-let array = []
-for (let i = 0; i < quantidadeCartas / 2; i++) {
-  array.push(imagens[i])
-  array.push(imagens[i])
-}
-array.sort(comparador)
-
-let cartas = []
-for (let i = 0; i < array.length; i++) {
-  document.querySelector('.container').innerHTML += `
+function criarArray() {
+  let imagens = [
+    '/images/bobrossparrot.gif',
+    '/images/explodyparrot.gif',
+    '/images/fiestaparrot.gif',
+    '/images/metalparrot.gif',
+    '/images/revertitparrot.gif',
+    '/images/tripletsparrot.gif',
+    '/images/unicornparrot.gif'
+  ]
+  imagens.sort(comparador)
+  let array = []
+  for (let i = 0; i < quantidadeCartas / 2; i++) {
+    array.push(imagens[i])
+    array.push(imagens[i])
+  }
+  array.sort(comparador)
+  for (let i = 0; i < array.length; i++) {
+    document.querySelector('.container').innerHTML += `
   <div class="card${i}" onclick="virarCarta(this)">
     <div class="face">
       <img src="/images/front.png" />
@@ -55,46 +55,38 @@ for (let i = 0; i < array.length; i++) {
     </div>
   </div>
   `
+  }
+  for (let i = 0; i < array.length; i++) {
+    let carta = [
+      { classe: undefined, img: undefined, index: undefined, valor: undefined }
+    ]
+    let card = document.querySelector(`.card${i}`)
+    carta.classe = card
+    carta.img = card
+      .querySelector('.back-face')
+      .querySelector('img')
+      .getAttribute('src')
+    carta.index = i
+    carta.valor = 0
+    cartas.push(carta)
+  }
 }
-for (let i = 0; i < array.length; i++) {
-  let carta = [
-    { classe: undefined, img: undefined, index: undefined, valor: undefined }
-  ]
-  let card = document.querySelector(`.card${i}`)
-  carta.classe = card
-  carta.img = card
-    .querySelector('.back-face')
-    .querySelector('img')
-    .getAttribute('src')
-  carta.index = i
-  carta.valor = 0
-  cartas.push(carta)
-}
-
-let clicks = 0
-let index1
-let index2
 function virarCarta(elemento) {
   clicks++
   if (clicks % 2 === 1) {
     for (let i = 0; i < cartas.length; i++) {
-      console.log('ola')
       if (elemento == cartas[i].classe) {
-        console.log('primeiro for')
         index1 = cartas[i].index
       }
     }
-    console.log('click impar')
     cartas[index1].classe.removeAttribute('onclick')
     cartas[index1].classe.classList.toggle('rotacionar180')
   } else if (clicks % 2 === 0) {
     for (let i = 0; i < cartas.length; i++) {
       if (elemento === cartas[i].classe) {
-        console.log('primeiro for')
         index2 = cartas[i].index
       }
     }
-    console.log('click par')
     cartas[index2].classe.removeAttribute('onclick')
     cartas[index2].classe.classList.toggle('rotacionar180')
   }
@@ -105,24 +97,11 @@ function virarCarta(elemento) {
         cartas[i].classe.removeAttribute('onclick')
       }
     }
-    console.log('cartas diferentes')
     setTimeout(delay, 1000)
   } else if (clicks % 2 === 0 && cartas[index1].img === cartas[index2].img) {
     cartas[index1].valor = 1
     cartas[index2].valor = 1
     verificarFim()
-  }
-}
-
-function delay() {
-  console.log('oi')
-  cartas[index1].classe.classList.toggle('rotacionar180')
-  cartas[index2].classe.classList.toggle('rotacionar180')
-  for (let i = 0; i < cartas.length; i++) {
-    if (cartas[i].valor === 0) {
-      console.log('adicionou o click')
-      cartas[i].classe.setAttribute('onclick', 'virarCarta(this)')
-    }
   }
 }
 function verificarFim() {
@@ -133,7 +112,6 @@ function verificarFim() {
     }
   }
   if (verificar === cartas.length) {
-    console.log('fim')
     clearInterval(meuIntervalo)
     const tempo = document.querySelector('h2').innerHTML
     setTimeout(
@@ -141,5 +119,24 @@ function verificarFim() {
       250,
       `Você ganhou em ${clicks} jogadas\nVocê ganhou em ${tempo} segundos`
     )
+    setTimeout(function () {
+      let resposta = prompt('Quer jogar novamente?')
+      if (resposta === 'sim') {
+        console.log('oi')
+        document.location.href = ''
+      }
+    }, 251)
   }
+}
+function delay() {
+  cartas[index1].classe.classList.toggle('rotacionar180')
+  cartas[index2].classe.classList.toggle('rotacionar180')
+  for (let i = 0; i < cartas.length; i++) {
+    if (cartas[i].valor === 0) {
+      cartas[i].classe.setAttribute('onclick', 'virarCarta(this)')
+    }
+  }
+}
+function comparador() {
+  return Math.random() - 0.5
 }
